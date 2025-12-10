56c5f2c17cdce1e5231aa6e68f26997bdd038dc1
mique-dev <miqueias.gt867@gmail.com>
2025-12-10 17:41:51 -0300

    10/12/25

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flassk, render_temmplate
+from flask import Flask, render_template
 
-app = Flask(__name)
+app = Flask(__name__)
 
-@app.rout("/")
+@app.route("/")
 def index():
-    return render_temmplate("index.html")
+    return render_template("index.html")
 
-if __name__ == "__name__":
+if __name__ == "__main__":
     app.run(debug=True)
